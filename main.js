f4a5a1b1b86659d23c5ec778c727ba5395800874
Pongkot Sae-li <pongkot.saeli@outlook.com>
2022-10-13 03:57:30 +0000

    chore: config

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-'use strict';
+'use strict'
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require('@hapi/hapi')
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
-  });
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost',
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => {
-      return 'Hello World!';
-    },
-  });
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            return 'Hello World!'
+        },
+    })
 
-  await server.start();
-  console.log('[acme-hapi-api]', 'Server running on %s', server.info.uri);
-};
+    await server.start()
+    console.log('[acme-hapi-api]', 'Server running on %s', server.info.uri)
+}
 
 process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
-});
+    console.log(err)
+    process.exit(1)
+})
 
-init();
+init()
