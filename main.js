29341f54d7927738a8f305957e28aa6ab10870e6
Pongkot Sae-li <pongkot.saeli@outlook.com>
2022-10-13 03:21:32 +0000

    chore: add prettierrc

--- main.js
+++ main.js
@@ -3,20 +3,26 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return 'Hello World!';
+    },
+  });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
